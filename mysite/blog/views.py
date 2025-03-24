787e3c82fcee7731152025ebdce125644f2364a8
EXCELSIOR <excelsior@mail.ru>
2025-03-24 18:11:01 +0300

    Добавление комментариев к постам

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import CommentForm
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 2
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -13,16 +23,30 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request,
-	          'blog/post/list.html',
-		  {'page': page,
-		   'posts': posts})
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-			     status='published',
-			     publish__year=year,
-			     publish__month=month,
-			     publish__day=day)
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
-		  'blog/post/detail.html',
-		  {'post': post})
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
